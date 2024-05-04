a612724aed073bdd9bbc124c7d32533734b758c8
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-04 21:00:44 +0200

    load serverRtpConfig in solidJS device

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,14 +12,22 @@ import * as mediasoupClient from 'mediasoup-client';
  */
 
 const App: Component = () => {
-	// Step 1: Create a device
-	const device = new mediasoupClient.Device();
-	console.log('device', device);
-
-	// Step 2: Load ServerRtpCapabilities
+	// Step 1: Load ServerRtpCapabilities
 	const [serverRtpCapabilities] = createResource(async () => {
 		const response = await fetch('http://localhost:3000/getServerRtpCapabilities');
 		return response.json();
+	});
+
+	const [device] = createResource(serverRtpCapabilities, async () => {
+		// Step 2: Create a device
+		const device = new mediasoupClient.Device();
+
+		//Step 3: Call device.load with serverRtpCapabilities
+		await device.load({routerRtpCapabilities: serverRtpCapabilities()});
+
+		console.log('device', device);
+
+		return device;
 	});
 
 	const debugServerRtpCapabilities = () => JSON.stringify(serverRtpCapabilities(), null, 2);
