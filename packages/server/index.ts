db5679506be94031988fa2cc9abeb155f3e8bfd6
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-04 20:40:41 +0200

    add /getServerRtpCapabilities endpoint in "server"

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,39 @@
 import { createApp, defineEventHandler, defineWebSocketHandler } from "h3";
+import { mediasoupServerPromise } from "./mediasoup/mediasoupServer";
 
-export const app = createApp();
+export const app = createApp({});
 
+// open: http://localhost:3000/
 app.use(
+  "/", // Root path
   defineEventHandler(() =>
     fetch(
       "https://raw.githubusercontent.com/unjs/crossws/main/examples/h3/public/index.html",
     ).then((r) => r.text())
   ),
+  {
+    match: (url) => url === "/",
+  },
+);
+
+// open: http://localhost:3000/getServerRtpCapabilities
+app.use(
+  "/getServerRtpCapabilities",
+  defineEventHandler(async () => {
+    const { router } = await mediasoupServerPromise;
+    return router.rtpCapabilities;
+  }),
 );
 
 app.use(
   "/_ws",
   defineWebSocketHandler({
-    open(peer) {
+    async open(peer) {
       console.log("[ws] open", peer);
+      // 1. When client connects:
+      const soupServer = await mediasoupServerPromise;
+
+      // const router = soupServer.router.createD;
     },
 
     message(peer, message) {
