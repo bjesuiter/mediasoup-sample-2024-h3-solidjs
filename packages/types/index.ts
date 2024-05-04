e2e865124b8a6983ef2e77dae2f6f0e556cd8066
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-04 23:39:39 +0200

    fix type WebRtcTransportOptions

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,8 +1,8 @@
-import type { AppData } from "mediasoup/node/lib/types";
+import type { AppData, WebRtcTransportOptions } from "mediasoup/node/lib/types";
 
 export type BennyWebsocketEnvelope = {
   command: "WebRtcTransportOptions";
-  payload: AppData;
+  payload: WebRtcTransportOptions<AppData>;
 } | {
   command: "getWebRtcTransportOptions";
   payload: void;
