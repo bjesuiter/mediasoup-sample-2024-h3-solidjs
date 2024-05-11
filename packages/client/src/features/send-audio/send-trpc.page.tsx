d9ced214d8742e332d2b968f5515b612009c6829
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-11 17:35:08 +0200

    Step 1: Get ServerRtpCapabilities

--- packages/client/src/features/send-audio/send-trpc.page.tsx
+++ packages/client/src/features/send-audio/send-trpc.page.tsx
@@ -1,4 +1,19 @@
+import {createEffect, createResource} from 'solid-js';
+import {trpcClient} from '../../trpc/trpc-client';
+
 export function SendTrpcPage() {
+	// Step 1: Get ServerRtpCapabilities
+	const [serverRtpCapabilities] = createResource(async () => {
+		const serverRtpCapabilities = await trpcClient.getServerRtpCapabilities.query();
+		console.log('Step 1: serverRtpCapabilities', serverRtpCapabilities);
+		return serverRtpCapabilities;
+	});
+
+	// debug effect to force recomputation of resources
+	createEffect(() => {
+		serverRtpCapabilities();
+	});
+
 	return (
 		<div>
 			<h1>Send Audio (with trpc + mediasoup)</h1>
