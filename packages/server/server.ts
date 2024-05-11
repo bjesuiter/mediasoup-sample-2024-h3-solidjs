cc9dbdc1b9f133f881f771fc8587ff65e220ecd2
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-11 17:18:50 +0200

    refactor express server

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,5 +1,6 @@
 import { expressApp } from "./express/express.app";
 
-expressApp.listen(4000);
+const expressPort = 4000;
 
-console.info(`Express App listening on http://localhost:4000/`);
+expressApp.listen(expressPort);
+console.info(`Express App listening on ${expressPort}`);
