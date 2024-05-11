7d66ba2ec27281ad8063752ec0a75ad21eda4df2
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-11 17:37:04 +0200

    Step 2: create device in send-trpc page

--- packages/client/src/features/send-audio/send-trpc.page.tsx
+++ packages/client/src/features/send-audio/send-trpc.page.tsx
@@ -1,5 +1,6 @@
 import {createEffect, createResource} from 'solid-js';
 import {trpcClient} from '../../trpc/trpc-client';
+import * as mediasoupClient from 'mediasoup-client';
 
 export function SendTrpcPage() {
 	// Step 1: Get ServerRtpCapabilities
@@ -9,9 +10,29 @@ export function SendTrpcPage() {
 		return serverRtpCapabilities;
 	});
 
+	// Step 2 & 3: Create device and load serverRtpCapabilities into it
+	const [device] = createResource(
+		() => ({
+			serverRtpCaps: serverRtpCapabilities(),
+		}),
+		async ({serverRtpCaps}) => {
+			if (!serverRtpCaps) return;
+
+			// Step 2: Create a device
+			const device = new mediasoupClient.Device();
+
+			//Step 3: Call device.load with serverRtpCapabilities
+			await device.load({routerRtpCapabilities: serverRtpCaps});
+
+			console.log('Step 2 & 3: device', device);
+
+			return device;
+		}
+	);
+
 	// debug effect to force recomputation of resources
 	createEffect(() => {
-		serverRtpCapabilities();
+		device();
 	});
 
 	return (
