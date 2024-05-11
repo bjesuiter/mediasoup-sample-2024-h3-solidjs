2a004ed1940bd40da13ec541b600026bdda26310
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-05-11 17:34:21 +0200

    add cors to express app

--- packages/server/express/express.app.ts
+++ packages/server/express/express.app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import { createContext } from "../trpc/trpc.base";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "../trpc/trpc.router";
+import cors from "cors";
 
 export const expressApp = express();
+
+expressApp.use(cors());
 
 expressApp.use(
   "/trpc",
